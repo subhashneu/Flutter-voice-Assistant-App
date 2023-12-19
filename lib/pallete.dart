cab277346b63fb8e7cfa6dfc2451fa073e229445
subhash <neupanesubhash123@gmail.com>
2023-12-19 19:04:42 +0545

    added red color

--- lib/pallete.dart
+++ lib/pallete.dart
@@ -10,4 +10,6 @@ class Pallete {
   static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
   static const Color blackColor = Colors.black;
   static const Color whiteColor = Colors.white;
+  static const Color redColor = Colors.white;
+
 }
